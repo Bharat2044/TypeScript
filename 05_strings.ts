5444a86f3b4f83550fee3bf3fb380c0ae830a9ff
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-02-19 16:46:29 +0000

    code added

---
+++ 05_strings.ts
@@ -0,0 +1,23 @@
+let m: string = "Bharat";
+
+console.log(typeof m, m);
+
+console.log(m.length);
+console.log(m[0]);
+console.log(m[9]);
+console.log(m[-1]);
+console.log(m[5]);
+
+console.log();
+
+console.log(m.charAt(0));
+console.log(m.charAt(9));
+console.log(m.charAt(-2));
+console.log(m.charAt(4));
+
+console.log();
+
+console.log(m.toLowerCase());
+console.log(m.toUpperCase());
+
+
