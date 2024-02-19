ac7b0eb609a8cd669e57dd36a8fc5a42f185e491
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-02-19 16:46:11 +0000

    code added

---
+++ 01_first.ts
@@ -0,0 +1,17 @@
+console.log("This is My TypeScript Code.");
+
+// TypeScript code goes here
+let firstName: string = "Bharat";
+let age: number = 21;
+
+console.log(typeof(firstName));
+console.log("Name = " + firstName);
+console.log(typeof(age));
+console.log("Age = " + age);
+
+
+class Test {
+
+}
+
+let obj: Test = new Test();
