4835620c1e0d9537c9638b120526ebec71508eaf
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-02 19:43:37 +0530

    code added

---
+++ 29_inheritance.ts
@@ -0,0 +1,47 @@
+/**
+ * Class A with a static variable 'x'
+ */
+class A {
+    // Static variable 'x' with an initial value
+    static x: number = 10;
+}
+
+// Access and log the static variable 'x' of class A
+console.log(A.x);
+
+
+/**
+ * Class B with a static variable 'x', an instance variable 'x', and static/instance methods
+ */
+class B {
+    // Static variable 'x' with an initial value
+    static x: number = 10;
+
+    // Instance variable 'x' with an initial value
+    x: number = 20;
+
+    // Static method 'test' for class B
+    static test() {
+        console.log("From class B static test() method");
+    }
+
+    // Instance method 'test' for class B
+    test() {
+        console.log("From class B instance test() method");
+    }
+}
+
+// Access and log the static variable 'x' of class B
+console.log(B.x);
+
+// Call the static method 'test' of class B
+B.test();
+
+// Instantiate an object of class B
+let b = new B();
+
+// Access and log the instance variable 'x' of object 'b'
+console.log(b.x);
+
+// Call the instance method 'test' of object 'b'
+b.test();
