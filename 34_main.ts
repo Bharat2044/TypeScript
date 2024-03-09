596473117ae60c0df67f248246fcf3f4f4a5c4b4
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-09 18:49:43 +0530

    code added

---
+++ 34_main.ts
@@ -0,0 +1,10 @@
+// import {x} from "./33_export_statement";
+// import {test} from "./33_export_statement";
+// import {A} from "./33_export_statement";
+
+import {x, test, A} from "./33_export_statement";
+
+console.log("x = " + x);
+test();
+
+let a = new A();
